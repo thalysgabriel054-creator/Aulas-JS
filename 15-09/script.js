21453fd58f1fd53ca6976f59766e72f7ba0efcb4
thalys <thalys@gmail.com>
2026-09-15 11:38:46 -0300

    adicionada pasta 15-09 - variaveis

---
+++ 15-09/script.js
@@ -0,0 +1,43 @@
+//let miranha = "peter parker";
+// numero = 999999999;
+//let numero 2 = "999999999";
+//let idade = 16
+// var mome = "thalys";
+// nome "pedro";
+
+ // const nome = "odirlei";
+ //nome = "Pedro";
+//idade = 20;
+//idade  = 21;
+//nome = "pedro";
+
+
+//console.log("o miranha e o" + miranha + " e ele tem " + idade + " anos")
+//crie 4 variaveis contendo : nome, sobrenome, idade e curso
+//exiba mo console em uma unica frase 
+
+let nome = "Thalys"
+let sobrenome = "Rabelo"
+let idade = 16
+ let curso = "desenvolvimento em java script";
+
+//console.log ("meu nome e" + nome + ", meu sobrenome e" + sobrenome + ", tenho" + idade + "anos e estou cursando " + curso)
+
+// let nome = "thalys"; //string
+// let peso = 58 ; //int
+// let altura = 1.70; // float
+// let funcionario = false ;//bool- true == verdadeiro ou false == falso  
+
+// let cidade = null;
+// let estado ;
+// console.log (typeof nome);
+// console.log(typeof peso);
+// console.log(typeof altura);
+// console.log(typeof funcionario);
+
+ console.log("a variavel nome tem " + nome + " e e do tipo " + typeof nome);
+console.log(" a variavel sobrenome tem" + sobrenome + " e e do tipo" + typeof sobrenome)
+console.log(" a variavel idade tem " + idade +"e e do tipo" + typeof idade )
+console.log("a variavel curso tem" + curso + " e e do tipo" + typeof curso)
+
+
